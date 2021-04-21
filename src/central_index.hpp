d1f669abf13f1969b53d2916f6042ba22cbeae81
William Harrell <wmharrel@ncsu.edu>
2021-04-21 15:08:47 -0400

    added comments to central_index.hpp

--- src/central_index.hpp
+++ src/central_index.hpp
@@ -6,12 +6,15 @@
 #include <list>
 
 
+/* Represents a client connected to the CI */
 typedef struct {
     std::string hostname;
     int upload_port;
 } ConnectedClient;
 
 
+/* Represents an association between a client and an RFC.
+ * As in, a client has a local copy of an RFC. */
 typedef struct {
     int rfc;
     std::string title;
@@ -19,6 +22,7 @@ typedef struct {
 } RFCHolder;
 
 
+/* Stores all of the data the central index needs to keep track of */
 class CentralIndex {
 
 private:
@@ -28,10 +32,17 @@ private:
     static std::mutex rfcs_mut;
 
 public:
+    /* Represents a client connecting to the CI */
     static bool add_client(std::string hostname, int upload_port);
+    
+    /* Represents a client disconnecting from the CI */
     static bool remove_client(std::string hostname, int upload_port);
 
+    
+    /* Represents a client stating they have an RFC */
     static bool add_rfc(std::string hostname, int upload_port, int rfc, std::string title);
+
+    /* Finds all the clients that have a local copy of an RFC */
     static std::list<ConnectedClient> find_rfcs(int rfc);
 
 };
